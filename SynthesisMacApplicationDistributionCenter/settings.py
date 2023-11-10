d5057707e91f4e4be5fb89e7f85c678229b3b7e1
vincent <vincentadamnemessis@gmail.com>
2023-11-10 20:40:11 +0800

    Append Redis as Cache instead of django default cache

--- SynthesisMacApplicationDistributionCenter/settings.py
+++ SynthesisMacApplicationDistributionCenter/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,9 +80,19 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'USERNAME': 'root',
+        'USER': 'root',
         'PASSWORD': 'ic3344',
         'NAME': 'SynthesisYouWantMacApplicationDistributionCenter',  # database Name
+    }
+}
+
+CACHES = {
+    'BACKEND': 'django_redis.cache.RedisCache',
+    'LOCATION': 'redis://127.0.0.1:6379',
+    'OPTIONS': {
+        "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        "CONNECTION_POOL_KWARGS": {"max_connections": 100}
+        # "PASSWORD": "123",
     }
 }
 
@@ -122,6 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
