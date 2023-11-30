d1a71554dfa11a41c2d2f3bff8532883b034b284
vincent <vincentadamnemessis@gmail.com>
2023-11-30 20:23:26 +0800

    register comments and posts'  models

--- commentsandposts/admin.py
+++ commentsandposts/admin.py
@@ -1,1 +1,16 @@
+from django.contrib import admin
+
+from commentsandposts.models import Comment, Article
+
+
 # Register your models here.
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'content', 'created_time', 'updated_time', 'parent']
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'title', 'content', 'created_time', 'updated_time']
