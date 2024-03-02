cd77931afa47b7bb7c8ce105cd228c1709b8f923
VincentAdamNemessis <vincentadamnemessis@gmail.com>
2024-03-02 23:23:39 +0800

    finished rank page and squared logo image

--- analytics/views.py
+++ analytics/views.py
@@ -1,4 +1,10 @@
+from datetime import datetime
+
 from django.shortcuts import render
+
+from general.common_compute import (get_hot_volume_of_article as g_h_a,
+                                    get_hot_volume_of_software as g_h_s)
+from general.init_cache import get_all_articles as g_a_as, get_all_software as g_a_s
 
 
 # Create your views here.
@@ -6,5 +12,78 @@ def index(request):
     return render(request, 'index.html')
 
 
+# @cache_page(60 * 15)
 def rank(request):
-    return render(request, 'rank.html')
+    all_articles, all_software = g_a_as(), g_a_s()
+    today_date = datetime.now().date()
+    # compute up and coming star of today
+    today_up_and_coming_star_articles, today_up_and_coming_star_software = \
+        ([article for article in all_articles if article.updated_time.date() == today_date],
+         [software for software in all_software if software.updated_time.date() == today_date])
+    today_up_and_coming_star_articles, today_up_and_coming_star_software = \
+        (sorted(today_up_and_coming_star_articles, key=lambda x: x.updated_time, reverse=True)[:10],
+         sorted(today_up_and_coming_star_software, key=lambda x: x.updated_time, reverse=True)[:10])
+    if len(today_up_and_coming_star_software) < 10:
+        today_up_and_coming_star_software += [None] * (10 - len(today_up_and_coming_star_software))
+    if len(today_up_and_coming_star_articles) < 10:
+        today_up_and_coming_star_articles += [None] * (10 - len(today_up_and_coming_star_articles))
+    today_popularity_articles, today_popularity_software = (all_articles, all_software)
+    for i in today_popularity_articles:
+        i.popularity = g_h_a(i, 2)
+    for i in today_popularity_software:
+        i.popularity = g_h_s(i, 2)
+
+    # compute popularity of today
+    today_popularity_articles, today_popularity_software = (
+        sorted(today_popularity_articles, key=lambda x: x.popularity, reverse=True)[:10],
+        sorted(today_popularity_software, key=lambda x: x.popularity, reverse=True)[:10])
+    for i in today_popularity_articles:
+        if i.popularity <= 0:
+            today_popularity_articles.remove(i)
+    for i in today_popularity_software:
+        if i.popularity <= 0:
+            today_popularity_software.remove(i)
+    if len(today_popularity_articles) < 10:
+        today_popularity_articles += [None] * (10 - len(today_popularity_articles))
+    if len(today_popularity_software) < 10:
+        today_popularity_software += [None] * (10 - len(today_popularity_software))
+
+    # compute up and coming star of all time
+    all_time_up_and_coming_star_articles, all_time_up_and_coming_star_software = (all_articles, all_software)
+    all_time_up_and_coming_star_articles, all_time_up_and_coming_star_software = (
+        sorted(all_time_up_and_coming_star_articles, key=lambda x: x.updated_time, reverse=True)[:10],
+        sorted(all_time_up_and_coming_star_software, key=lambda x: x.updated_time, reverse=True)[:10])
+    if len(all_time_up_and_coming_star_software) < 10:
+        all_time_up_and_coming_star_software += [None] * (10 - len(all_time_up_and_coming_star_software))
+    if len(all_time_up_and_coming_star_articles) < 10:
+        all_time_up_and_coming_star_articles += [None] * (10 - len(all_time_up_and_coming_star_articles))
+
+    # compute popularity of all time
+    all_time_popularity_articles, all_time_popularity_software = (all_articles, all_software)
+    for i in all_time_popularity_articles:
+        i.popularity = g_h_a(i, 1)
+    for i in all_time_popularity_software:
+        i.popularity = g_h_s(i, 1)
+    all_time_popularity_articles, all_time_popularity_software = (
+        sorted(all_time_popularity_articles, key=lambda x: x.popularity, reverse=True)[:10],
+        sorted(all_time_popularity_software, key=lambda x: x.popularity, reverse=True)[:10])
+    for i in all_time_popularity_articles:
+        if i.popularity <= 0:
+            all_time_popularity_articles.remove(i)
+    for i in all_time_popularity_software:
+        if i.popularity <= 0:
+            all_time_popularity_software.remove(i)
+    if len(all_time_popularity_articles) < 10:
+        all_time_popularity_articles += [None] * (10 - len(all_time_popularity_articles))
+    if len(all_time_popularity_software) < 10:
+        all_time_popularity_software += [None] * (10 - len(all_time_popularity_software))
+    return render(request, 'rank.html', {
+        'today_up_and_coming_star_articles': today_up_and_coming_star_articles,
+        'today_up_and_coming_star_software': today_up_and_coming_star_software,
+        'today_popularity_articles': today_popularity_articles,
+        'today_popularity_software': today_popularity_software,
+        'all_time_up_and_coming_star_articles': all_time_up_and_coming_star_articles,
+        'all_time_up_and_coming_star_software': all_time_up_and_coming_star_software,
+        'all_time_popularity_articles': all_time_popularity_articles,
+        'all_time_popularity_software': all_time_popularity_software,
+    })
--- general/common_compute.py
+++ general/common_compute.py
@@ -1,35 +1,56 @@
-from commentswitharticles.models import Article
-from software.models import SoftWare
+from datetime import datetime, time
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-def get_hot_volume_of_article(article_id):
+def get_hot_volume_of_article(article, get_type=1):
     hot_volume: int = 0
     try:
-        article = Article.objects.get(id=article_id, state=2)
-        hot_volume += article.updated_time.timestamp() / 1000000 + 10
-        hot_volume += article.view_volume * 10 + article.thumbs_volume * 50
-        hot_volume += article.comment_set.count() * 200
-        hot_volume += article.correlation_software.download_volume * 30
+        if get_type == 1:
+            hot_volume += article.updated_time.timestamp() / 1000000 + 10
+            hot_volume += article.view_volume * 10 + article.thumbs_volume * 50
+            hot_volume += article.comment_set.count() * 200
+            hot_volume += article.correlation_software.download_volume * 30
+        if get_type == 2:
+            today = datetime.now().date()
+            start_time = time(0, 0, 0)  # 设置开始时间为 00:00:00
+            end_time = time(23, 59, 59)  # 设置结束时间为 23:59:59
+            start_datetime = datetime.combine(today, start_time)  # 组合日期和时间，得到开始日期时间
+            end_datetime = datetime.combine(today, end_time)  # 组合日期和时间，得到结束日期时间
+            hot_volume += (article.updated_time.timestamp() / 1000000 + 10) \
+                if article.updated_time.date() == datetime.now().date() else 0
+            hot_volume += article.comment_set.all().filter(
+                created_time__range=(start_datetime, end_datetime)).count() * 200
     except ValueError:
         pass
     except TypeError:
         pass
     except AttributeError:
         pass
+    except IndexError:
+        pass
     return int(hot_volume)
 
 
-def get_hot_volume_of_software(software_id):
+def get_hot_volume_of_software(software, get_type=1):
     hot_volume: int = 0
     try:
-        software = SoftWare.objects.get(id=software_id, state=2)
-        hot_volume += software.updated_time.timestamp() / 1000000 + 10
-        hot_volume += software.view_volume * 10 + software.thumbs_volume * 50
-        hot_volume += software.download_volume * 200
-        hot_volume += software.comment_set.count() * 100
-        hot_volume += software.article_set.count() * 20
+        if get_type == 1:
+            hot_volume += software.updated_time.timestamp() / 1000000 + 10
+            hot_volume += software.view_volume * 10 + software.thumbs_volume * 50
+            hot_volume += software.download_volume * 200
+            hot_volume += software.comment_set.count() * 100
+            hot_volume += software.article_set.count() * 20
+        if get_type == 2:
+            today = datetime.now().date()
+            start_time = time(0, 0, 0)
+            end_time = time(23, 59, 59)
+            start_datetime = datetime.combine(today, start_time)
+            end_datetime = datetime.combine(today, end_time)
+            hot_volume += (software.updated_time.timestamp() / 1000000 + 10) \
+                if software.updated_time.date() == datetime.now().date() else 0
+            hot_volume += software.comment_set.all().filter(
+                created_time__range=(start_datetime, end_datetime)).count() * 100
     except ValueError:
         pass
     except TypeError:
